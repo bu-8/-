88a93c3b0b12fa9510147d9fad88fbf18f411c4e
bu-8 <110326761+bu-8@users.noreply.github.com>
2022-09-03 04:16:23 +0800

    Delete react-tesla-shop-master directory

--- react-tesla-shop-master/src/pages/Find/store/constants.js
+++
@@ -1,1 +0,0 @@
-export const CHANGE_FINDDATA = 'find/CHANGE_FINDDATA';
--- react-tesla-shop-master/src/pages/Tesla/store/constants.js
+++
@@ -1,4 +0,0 @@
-export const CHANGE_TESLADATA = 'tesla/CHANGE_TESLADATA';
-export const CHANGE_INDEX = 'tesla/CHANGE_INDEX';
-export const SET_COLORINDEX = 'teslaInfo/model/SET_COLORINDEX';
-export const SET_WHEELINDEX = 'teslaInfo/model/SET_WHEELINDEX';
